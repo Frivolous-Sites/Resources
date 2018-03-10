9deda60b27901fdac52a2e516a174855345c2709
Frivolous-Sites <35827420+Frivolous-Sites@users.noreply.github.com>
2018-03-10 02:23:55 -0500

    Create Members Login Click to Submit.js

---
+++ JavaScript/Members Login Click to Submit.js
@@ -0,0 +1,9 @@
+$("#login-button").click(function(event){
+		 event.preventDefault();
+	 
+	 $('form').fadeOut(500);
+	 $('.wrapper').addClass('form-success');
+});
+
+
+// Copyright 2018 Resources (resources.ga). All rights reserved.
